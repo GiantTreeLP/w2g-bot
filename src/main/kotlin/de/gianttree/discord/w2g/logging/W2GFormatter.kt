b80d27e2e5ee966dfe470bb4a42d74b3551a0903
Marvin <marv.1997@live.de>
2021-01-13 21:16:48 +0100

    Improve logger

--- src/main/kotlin/de/gianttree/discord/w2g/logging/W2GFormatter.kt
+++ src/main/kotlin/de/gianttree/discord/w2g/logging/W2GFormatter.kt
@@ -3,6 +3,8 @@ package de.gianttree.discord.w2g.logging
 import java.io.PrintWriter
 import java.io.StringWriter
 import java.text.DateFormat
+import java.time.*
+import java.time.format.DateTimeFormatter
 import java.util.*
 import java.util.logging.LogRecord
 import java.util.logging.SimpleFormatter
@@ -10,14 +12,12 @@ import java.util.logging.SimpleFormatter
 class W2GFormatter : SimpleFormatter() {
     private val sw = StringWriter()
     private val pw = PrintWriter(sw)
-    private val date = Date()
-    private val dateFormatter = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.MEDIUM)
+    private val dateFormatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME
     override fun format(record: LogRecord): String {
         return buildString {
             append(record.level.localizedName)
             append(' ')
-            date.time = record.millis
-            append(dateFormatter.format(date))
+            append(dateFormatter.format(Instant.ofEpochMilli(record.millis).atZone(ZoneId.systemDefault())))
             append(": ")
             append(formatMessage(record))
             if (record.thrown != null) {
