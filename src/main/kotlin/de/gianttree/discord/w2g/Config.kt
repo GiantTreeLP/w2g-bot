fb7b4e3818d2f753cc5cda517d688d60de297b8b
Marvin <marv.1997@live.de>
2022-09-08 18:26:54 +0200

    Expand configuration in preparation for database connectivity and to better accommodate custom environments/other guilds

--- src/main/kotlin/de/gianttree/discord/w2g/Config.kt
+++ src/main/kotlin/de/gianttree/discord/w2g/Config.kt
@@ -1,11 +1,16 @@
+@file:OptIn(ExperimentalTime::class)
+
 package de.gianttree.discord.w2g
 
+import dev.kord.common.serialization.DurationInSeconds
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import java.io.File
+import kotlin.time.Duration.Companion.minutes
+import kotlin.time.ExperimentalTime
 
 private val json = Json {
     encodeDefaults = true
@@ -18,7 +23,25 @@ data class Config(
     val debugMode: Boolean = false,
     val discordToken: String = "YOUR_DISCORD_TOKEN_HERE",
     val w2gToken: String = "YOUR_W2G_TOKEN_HERE",
-    val httpPort: Int = 12345
+    val httpPort: Int = 12345,
+    val intervals: Intervals = Intervals(),
+    val databaseConnection: DatabaseConnection = DatabaseConnection(),
+)
+
+@Serializable
+data class DatabaseConnection(
+    val jdbcUrl: String = "jdbc:mariadb://localhost:3306/w2g-bot",
+    val driver: String = "org.mariadb.jdbc.Driver",
+    val username: String = "w2g-bot",
+    val password: String = "w2g-bot",
+    val maxPoolSize: Int = 2,
+    val minIdle: Int = 1,
+)
+
+@Serializable
+data class Intervals(
+    val presenceInterval: DurationInSeconds = 5.minutes,
+    val guildMemberUpdateInterval: DurationInSeconds = 2.minutes,
 )
 
 @ExperimentalSerializationApi
