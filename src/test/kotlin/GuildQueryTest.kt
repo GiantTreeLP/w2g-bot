0d91a8c81d6249cf921cb9672d9b5ab018339c0b
Marvin <marv.1997@live.de>
2022-09-10 13:37:12 +0200

    Fix GuildQueryTest by clearing/flushing the table and inserting a test entry

--- src/test/kotlin/GuildQueryTest.kt
+++ src/test/kotlin/GuildQueryTest.kt
@@ -1,10 +1,15 @@
 package de.gianttree.discord.w2g
 
+import de.gianttree.discord.w2g.database.Guild
 import de.gianttree.discord.w2g.database.Guilds
 import de.gianttree.discord.w2g.database.setupDatabaseConnection
+import de.gianttree.discord.w2g.database.suspendedInTransaction
+import dev.kord.common.entity.Snowflake
 import kotlinx.coroutines.runBlocking
+import kotlinx.datetime.Clock
 import kotlinx.serialization.ExperimentalSerializationApi
 import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.deleteAll
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
@@ -24,6 +29,15 @@ class GuildQueryTest {
         val config = readConfig()
         runBlocking {
             this@GuildQueryTest.database = setupDatabaseConnection(config, Logger.getAnonymousLogger())
+            suspendedInTransaction(this@GuildQueryTest.database) {
+                Guilds.deleteAll()
+                Guild.new(Snowflake(0)) {
+                    this.name = "Test Guild"
+                    this.active = true
+                    this.approxMemberCount = 0
+                    this.lastUpdate = Clock.System.now().toEpochMilliseconds()
+                }
+            }
         }
     }
 
